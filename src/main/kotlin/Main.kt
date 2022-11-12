d9182bba3d1ca2512ec5dcc1010bcc146435c3b2
tosmo5 <tosmo5@163.com>
2022-11-12 09:48:52 +0800

    publish test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,6 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import com.tosmo.koffice.Hellor
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    val obj = Hellor()
+    obj.sayHello()
 }
